935007c6f73ffe0cbc92640cb69de0f18847b5bd
Daniel Shimield <dxshim90@gmail.com>
2019-02-01 11:22:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,11 +63,10 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-programmer_hash[::yukihiro_matsumoto] = {
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+programmer_hash[:yukihiro_matsumoto] = {
+:known_for => "Ruby",
+:languages => ["LISP", "C"]
+}
 
 	programmer_hash = 
  		{
